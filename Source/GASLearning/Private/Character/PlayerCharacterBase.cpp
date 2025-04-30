8327a00e0b870f44a9af3ff2b1dbe8f26fea31dd
gecaiming <gecaiming@piesat.cn>
2025-04-30 15:35:16 +0800

    palyer蓝图和哥布林蓝图创建

--- Source/GASLearning/Private/Character/PlayerCharacterBase.cpp
+++ Source/GASLearning/Private/Character/PlayerCharacterBase.cpp
@@ -7,6 +7,9 @@ APlayerCharacterBase::APlayerCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision); 
 }
 
 void APlayerCharacterBase::BeginPlay()
